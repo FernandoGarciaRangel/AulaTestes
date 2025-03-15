75fe8fce01095d62d7cdb1566f44843bb58ac162
Fernando Garcia <108248511+FernandoGarciaRangel@users.noreply.github.com>
2025-03-14 21:29:39 -0300

    Add files via upload

---
+++ calculadora/src/operacoes/Calculadora.java
@@ -0,0 +1,45 @@
+package operacoes;
+
+public class Calculadora {
+
+    /**
+     * Realiza a soma.
+     * @param valor1 primeiro valor.
+     * @param valor2 segundo valor.
+     * @return retorna valor.
+     */
+    public double somar(final double valor1, final double valor2) {
+        return valor1 + valor2;
+    }
+
+    /**
+     * Realiza a subtração.
+     * @param valor1 primeiro valor.
+     * @param valor2 segundo valor.
+     * @return retorna valor.
+     */
+    public double subtrair(final double valor1, final double valor2) {
+        return valor1 - valor2;
+    }
+
+    /**
+     * Realiza a divisão.
+     * @param valor1 primeiro valor.
+     * @param valor2 segundo valor.
+     * @return retorna valor.
+     */
+    public double dividir(final double valor1, final double valor2) {
+        return valor1 / valor2;
+    }
+
+    /**
+     * Realiza a multiplicação.
+     * @param valor1 primeiro valor.
+     * @param valor2 segundo valor.
+     * @return retorna valor.
+     */
+    public double multiplicar(final double valor1, final double valor2) {
+        return valor1 * valor2;
+    }
+}
+
